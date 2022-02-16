3ac1071b02a5e9addd6fdda710c20d4dd65e66c0
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-17 00:03:16 +0100

    [add] action text

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,3 +6,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "js-cookie", to: "https://ga.jspm.io/npm:js-cookie@3.0.1/dist/js.cookie.mjs"
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.js"
