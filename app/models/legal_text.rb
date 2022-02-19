6421b67fe122fddbddc936c5a75c45294f85c3b5
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-19 22:12:23 +0100

    [add] validations to legal_text.rb

--- app/models/legal_text.rb
+++ app/models/legal_text.rb
@@ -2,5 +2,6 @@ class LegalText < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   has_rich_text :content
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true, length: { minimum: 10, message: :too_short  }
+  validates :content, presence: true
 end
