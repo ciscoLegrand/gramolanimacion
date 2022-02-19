a872791233fb11357341ff1c8ff4a3424c7f65e2
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-19 22:13:08 +0100

    [refactor] delete all autogenerate routes for legal text and transform it into a resources route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'legal_texts/index'
-  get 'legal_texts/show'
-  get 'legal_texts/new'
-  get 'legal_texts/edit'
-  get 'legal_texts/create'
-  get 'legal_texts/update'
-  get 'legal_texts/destroy'
-
   root "mainweb#index"
+
+  resources :legal_texts
 
 end
