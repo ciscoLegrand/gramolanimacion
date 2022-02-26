6560ea41801aa6b2943ab99737221bac996cffc0
ciscoLegrand <ciscco.glez@gmail.com>
2022-02-26 16:57:21 +0100

    [style] modify buttons style on tailwind components

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   resources :legal_texts
 
+  match "/cookies", to: 'mainweb#cookies', via: [:get, :post]
+
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 end
