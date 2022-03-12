0fface277d8f6f87e022424f8a858afb58360819
ciscoLegrand <ciscco.glez@gmail.com>
2022-03-12 19:19:43 +0100

    [add] site category and site legal text pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :error, :alert, :info, :notice
+
+  before_action :set_categories 
+
+  private 
+    def set_categories 
+      @categories = Category.all 
+    end
 end
--- app/controllers/mainweb_controller.rb
+++ app/controllers/mainweb_controller.rb
@@ -1,8 +1,21 @@
 class MainwebController < ApplicationController
+  before_action :set_category, only: %i[site_category]
   def index
     @contact = Contact.new
   end
 
+  def site_category
+  end
+  
+  def site_legal_text
+    @legal_text = LegalText.friendly.find(params[:legal_text_id])
+  end
+
   def cookie
   end
+
+  private 
+    def set_category 
+      @category = Category.friendly.find(params[:category_id])
+    end
 end
