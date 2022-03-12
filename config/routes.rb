c08c7662d853babdf789c73363507e191bc8197d
ciscoLegrand <ciscco.glez@gmail.com>
2022-03-12 19:23:19 +0100

    [feat] scope under /admin all resources rotes and exclude mainweb to public show

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
   root "mainweb#index"
 
-  resources :legal_texts
-  resources :contacts
+  match 'categorias/:category_id',     to: 'mainweb#site_category',     via: :get, as: :site_category
+  match 'textos/:legal_text_id',       to: 'mainweb#site_legal_text',   via: :get, as: :site_legal_text
+  
+  scope '/admin' do
+    resources :legal_texts
+    resources :contacts
+    resources :categories
 
-  resources :email_base_templates do 
-    resources :email_custom_templates, except: %w[show]
+    resources :email_base_templates do 
+      resources :email_custom_templates, except: %w[show]
+    end 
   end
 
-  match "/cookies", to: 'mainweb#cookies', via: [:get, :post]
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
+  match "/cookies", to: 'mainweb#cookies',              via: [:get, :post]
+  match "/404",     to: "errors#not_found",             via: :all
+  match "/500",     to: "errors#internal_server_error", via: :all
 end
