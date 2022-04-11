add73908a0c05322665a0161dd7cc0cb5be74af9
ciscoLegrand <cisco.glez@gmail.com>
2022-04-11 14:00:13 +0200

    [add] stock to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_15_192211) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_114932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -119,6 +119,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_15_192211) do
     t.string "slug", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stock"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
