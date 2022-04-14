43973bb96f604feb38b1439fa35ed47298097787
ciscoLegrand <cisco.glez@gmail.com>
2022-04-14 16:56:44 +0200

    [style] mobile menu dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-	static targets = [ "productfeatures" ]
+	static targets = [ "productfeatures", 'mobilemenu' ]
+
+	display_menu(e) {
+		console.log('holamundo')
+		e.preventDefault()
+		this.mobilemenuTarget.classList.contains("hidden") ?
+			this.mobilemenuTarget.classList.remove("hidden") :
+			this.mobilemenuTarget.classList.add("hidden")
+	}
 
 	display_product_features(e) {
 		e.preventDefault()
